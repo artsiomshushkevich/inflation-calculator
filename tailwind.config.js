9e8e5c64c18c00d28e0ae4ca4496fe915c24e06c
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2025-05-27 17:44:38 +0300

    fix: update Tailwind content paths for proper style processing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,8 @@
 export default {
   darkMode: ["class"],
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   prefix: "",
   theme: {
