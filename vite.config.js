dff8ef5450f5def6bda1b87de969417d8f2e537c
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2025-05-28 11:24:35 +0300

    Update build config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Generate a random salt for each build
+const buildSalt = Date.now().toString() + Math.random().toString(36).substring(2, 10)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/inflation-calculator/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${buildSalt}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${buildSalt}.js`,
+        assetFileNames: `assets/[name]-[hash]-${buildSalt}.[ext]`,
+      },
+    },
+  },
 })
